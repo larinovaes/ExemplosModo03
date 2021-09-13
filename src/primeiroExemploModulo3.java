60f7fcff7222cd1d35369d0a571770dd922c1007
Larissa Novais <larinovais54@gmail.com>
2021-09-13 13:41:24 -0300

    exercicio modulo03

--- src/primeiroExemploModulo3.java
+++ src/primeiroExemploModulo3.java
@@ -17,17 +17,41 @@ public class primeiroExemploModulo3 {
         Map<String, Double> produtos = new HashMap<String, Double>();
 
         boolean repeticao = true;
-
-        System.out.println("Vamos iniciar nossa lista de produtos comprados.");
-
-        while (repeticao){
-            System.out.println("Por favor digite o nome do seu produto");
-            String nome = leitor.nextLine();
-            System.out.println("Por favor informe o preço do produto selecionado");
-            Double preco = leitor.nextDouble();
+        int menu = 0;
 
 
+        while (repeticao) {
+
+            System.out.println("--- BEM VINDO AO SUPERMERCADO NOVAIS ---");
+            System.out.println("Para adicionar produtos no carrinho digite 1 ");
+            System.out.println("Para exebir lista de produtos adicionados digite 2");
+            System.out.println("Para excluir produtos da lista digite 3");
+            menu = leitor.nextInt();
+            leitor.nextLine();
+
+            switch (menu) {
+                case 1:
+                    System.out.println("Por favor digite o nome do seu produto");
+                    String nomeDoProduto = leitor.nextLine();
+                    System.out.println("Por favor informe o preço do produto selecionado");
+                    Double preco = leitor.nextDouble();
+                    System.out.println("Produto adicionado ao carrinho!");
+                    produtos.put(nomeDoProduto,preco);
+                    break;
+
+                case 2:
+                    for (String listaDeProdutos: produtos.keySet()) {
+                        System.out.println("Todos os produtos selecionados: ");
+                        System.out.println("Produto: " + listaDeProdutos + "\nPreço: " + produtos.get(listaDeProdutos));
+                    }
+                    break;
+                case 3:
+                    System.out.println("Digite o nome do produto que deseja remover da lista");
+                    String excluirProduto = leitor.nextLine();
+                    produtos.remove(excluirProduto);
+                    System.out.println("Produto removido.");
+                    break;
+            }
         }
-
     }
 }
