ab14d159e2bf4c384c7d6826db1ccacc9fa66a90
Larissa Novais <larinovais54@gmail.com>
2021-09-14 10:50:19 -0300

    começando terceiro execicio

---
+++ src/TerceiroExercicioModulo03.java
@@ -0,0 +1,12 @@
+/**
+ * Crie um programa para gerenciar o cardápio de um restaurante. Cada prato tem um nome e pode
+ * conter diversos ingredientes. No final do programa, o usuário deve decidir se irá adicionar mais
+ * de um prato, exibir todos os pratos cadastrados ou excluir um prato pelo nome.
+ */
+
+public class TerceiroExercicioModulo03 {
+
+    public static void main(String[] args) {
+
+    }
+}
